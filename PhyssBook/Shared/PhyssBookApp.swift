7938617aa7502187a289ec9469a4c70ca2882870
Leyo165 <yohan.nicolay@gadz.org>
2022-04-11 04:32:39 +0200

    Test de xcode

---
+++ PhyssBook/Shared/PhyssBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhyssBookApp.swift
+//  Shared
+//
+//  Created by Elie Roussarie on 11/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhyssBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
