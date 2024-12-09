1becdf5f9e6390093b2e370c6600f9ebe8cfc659
L165 <yohan.nicolay@gadz.org>
2024-12-09 04:02:17 +0100

    Reapply "Docker init"
    This reverts commit cdbf794ec57b1509c734ea548d584e90342b878e.

--- Front-End/Images/icon.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import Ionicons from '@expo/vector-icons/Ionicons'
-
-export function getIcon(name,action){
-    return <Ionicons onChange = {() => action} name = {name} size = {25}/>
-    
-}
