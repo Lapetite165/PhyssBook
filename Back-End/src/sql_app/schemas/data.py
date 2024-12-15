6a648a77117aa4c4539c341589dcd695a3484b80
L165 <yohan.nicolay@gadz.org>
2024-12-15 13:57:39 +0100

    Concentrate on Front-end

--- Back-End/src/sql_app/schemas/data.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from src.sql_app.schemas.asso_bools import AssoBools
-from src.sql_app.schemas.posts import PostsBase
-from .users import Users
-
-
-class Data(Users):
-    posts: List[PostsBase]
-    asso_bools: List[AssoBools]
